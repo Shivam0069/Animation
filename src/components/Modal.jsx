bbd14005e15c0eb9b1378bd3c7aca8e4e286a540
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2023-10-07 00:40:18 +0530

    Framer Motion Disappearances

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,8 +5,9 @@ export default function Modal({ title, children, onClose }) {
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
-        initial={{ opacity: 0, y: -30 }}
+        initial={{ opacity: 0, y: -100 }}
         animate={{ opacity: 1, y: 0 }}
+        exit = {{opacity : 0 , y : 100}}
         open
         className="modal"
       >
